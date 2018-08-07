7d6d6272b0fab87ac9c0a1722831a0067ea35fc2
Zoilo Granda <zgranda@gmail.com>
2018-08-07 17:42:06 -0400

    Improved styles Used facebook login only screen

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -15,11 +15,12 @@ export class HomePage {
       this.facebook.api('me?fields=id,name,email,first_name,picture.width(720).height(720).as(picture_large)',[])
       .then(profile=>{
         this.userData= {
-          email:profile['mail'],
+          email:profile['email'],
           first_name:profile['first_name'],
           picture:profile['picture_large']['data']['url'],
           username:profile['name']
         }
+        console.log(this.userData)
       })
     })
   }
